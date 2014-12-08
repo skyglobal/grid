8aac78078e53718fe8cf7f5135263bafd07252a8
Sabeur Thabti <thabti.sabeur@gmail.com>
2014-12-08 15:13:52 +0000

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var pkg = require('./package.json');
+var skyComponentHelper = require('gulp-sky-component-helper')(gulp, pkg);
+var paths = skyComponentHelper.paths;
+var runSequence = require('run-sequence');
+
+gulp.task('pre-build', function(cb){
+    //example pre-build task, which is automatically part of the build process
+    return runSequence('custom-gulp-task-1', cb);
+});
+
+gulp.task('custom-gulp-task-1', function(cb){
+    //exmape empty task
+    return cb();
+});
