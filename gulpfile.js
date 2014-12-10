70eacbd0eb4454958423ae8d58ee6e431bd640a0
Peter Mouland <peter.mouland@gmail.com>
2014-12-10 22:26:11 +0000

    update gulp-component-helper + add link to github on demo page

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,6 @@
 'use strict';
 
 var gulp = require('gulp');
-var pkg = require('./package.json');
-var skyComponentHelper = require('gulp-sky-component-helper')(gulp, pkg);
-var paths = skyComponentHelper.paths;
+var componentHelper = require('gulp-component-helper')(gulp);
+var paths = componentHelper.paths;
 var runSequence = require('run-sequence');
-
-gulp.task('pre-build', function(cb){
-    //example pre-build task, which is automatically part of the build process
-    return runSequence('custom-gulp-task-1', cb);
-});
-
-gulp.task('custom-gulp-task-1', function(cb){
-    //exmape empty task
-    return cb();
-});
